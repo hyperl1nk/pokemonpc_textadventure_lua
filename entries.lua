e15eb4ceb156ec6f27facf0ee45bce1caee70d7f
hyperl1nk <35341069+hyperl1nk@users.noreply.github.com>
2018-03-28 14:12:01 -0600

    Create entries.lua

---
+++ entries.lua
@@ -0,0 +1,26 @@
+
+“pk1” = {
+   typ=“pkmn”,
+   name=“lee”,
+   pokedex={448,”B”}
+   biography=“Lee is a peculiar Lucario, found in the Alola region on Route 12. He can speak telepathically, but is mute otherwise.”,
+   held_item={“Trainer Card”,”This Pokémon insists on holding onto it, so its details are unknown.”,secret=“It belongs to Porter Ballou, a trainer with eight Z-Crystals.”}
+}
+
+“pk2” = {
+   typ=“pkmn”,
+   name=“Galpal”,
+   pokedex={9999,”ERR”},
+   biography=“Galpal was found on a beach on [CORRUPTED].”,
+   held_item={“Psychium Z”,”A crystallized form of Z Power. It’s scuffed and scratched here and there, but still works fine.”,secret””}
+}
+
+
+
+“pk3” = {
+   typ=“pkmn”,
+   name=“Incineroar”,
+   pokedex={3,”B”},
+   biography=“Found in an unidentified area”,
+   held_item={“UniZ”,”A small cube, partially rounded by wear.”,secret=“There’s nothing else to know about this item.”
+   }
